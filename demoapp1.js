7dc45d752ed9f11f0e0b9844ca99c5e2e2051234
Naoyuki Sakai <naoyuki.sakai@gmail.com>
2014-12-15 22:04:30 +0900

    LED lightening demo

---
+++ demoapp1.js
@@ -0,0 +1,16 @@
+ZGN(function() {
+
+    var term = ZGN.term(1);
+
+    var pinNo = '18';
+
+    term.gpio.pinMode(pinNo, ZGN.OUTPUT);
+
+    $(document).on('click', '#on', function() {
+        term.gpio.digitalWrite(pinNo, ZGN.HIGH);
+    });
+
+    $(document).on('click', '#off', function() {
+        term.gpio.digitalWrite(pinNo, ZGN.LOW);
+    });
+});
