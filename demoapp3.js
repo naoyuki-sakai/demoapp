1998c0bdddf098e3c802afaba1767340f93edad9
naoyuki-sakai <naoyuki.sakai@gmail.com>
2014-12-26 11:07:01 +0900

    mod lack of arguments

--- demoapp3.js
+++ demoapp3.js
@@ -29,7 +29,7 @@ ZGN(function() {
 
 	
 	// まずモーターを止める
-	modPinValue(0, 0, 0, 0);
+	modPinValue(0, 0, 0, 0, 1.0);
 
     // タッチエリアの設定
     var touchArea = $("#touchArea")[0];
@@ -44,18 +44,19 @@ ZGN(function() {
 	
 
 	function modPinValue(in4, in3, in2, in1, rate) {
-		
+		// モータードライバー入力端子への出力
 		digitOutput('4', in4);
 		digitOutput('3', in3);
 		digitOutput('2', in2);
 		digitOutput('1', in1);
 
+
 		rate4Prev = rate*in4;
 		rate3Prev = rate*in3;
 		rate2Prev = rate*in2;
 		rate1Prev = rate*in1;
-		
-		
+
+		// PWM 制御端子への出力
 		pwmOutput('19', rate);
 		pwmOutput('18', rate);
 	}
@@ -63,11 +64,11 @@ ZGN(function() {
 	function digitOutput(pinNo, digitIn){
 		if ( digitIn == 1 ) { 
 			term.gpio.digitalWrite(pinNo, ZGN.HIGH, function(){
-				console.log("pin " + pinNo + "DIGIT: HIGH");
+				console.log("pin " + pinNo + " DIGIT: HIGH");
 			});
 		} else {
 			term.gpio.digitalWrite(pinNo, ZGN.LOW, function(){
-				console.log("pin " + pinNo + "DIGIT: LOW");
+				console.log("pin " + pinNo + " DIGIT: LOW");
 			});
 		}
 	}
@@ -87,7 +88,6 @@ ZGN(function() {
 
 	    if(touch.pageX<width/3){ // 左旋回
 	        var rate = maxSpeed*(width/3-touch.pageX)/(width/3);
-
 	        // 前回送信時と値が大きく違うときのみ送信
 	        if(Math.abs(rate-rate3Prev)>th || Math.abs(rate-rate2Prev)>th){
 	        	modPinValue(0, 1, 1, 0, rate);
@@ -126,7 +126,7 @@ ZGN(function() {
 	function touchEndEvent(e){
 	    e.preventDefault();
 
-		modPinValue(0, 0, 0, 0);
+		modPinValue(0, 0, 0, 0, 1.0);
 	}
 
 	// クリック時のイベントリスナー（主にPC用）
@@ -141,7 +141,7 @@ ZGN(function() {
 	    }else if(e.pageX<2*width/3){ // 前後移動
 	        // 左右の車輪の速さの違いの補正
 	        if(e.pageY>=2*height/5 && e.pageY<3*height/5){
-	        	modPinValue(0, 0, 0, 0);
+	        	modPinValue(0, 0, 0, 0, rate);
 	        }else if(e.pageY<height/2){
 	            var rate = maxSpeed*(height/2-e.pageY)/(height/2);
 				modPinValue(1, 0, 1, 0, rate);
